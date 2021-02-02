187534ab1aacee28c1fd0b5d432e8a45834ef463
philipjss <philip.schwarz@sefaira.com>
2021-02-02 19:48:13 +0000

    make test for one orange pass

--- src/test/scala/CheckoutSystem.scala
+++ src/test/scala/CheckoutSystem.scala
@@ -3,6 +3,7 @@ object CheckoutSystem {
   def checkout(items: List[String]): Either[String, Int] = items match {
     case Nil => Right(0)
     case List("apple") => Right(60)
+    case List("orange") => Right(25)
     case _ => Left(s"ERROR - the following items were not recognised: ${items.mkString("'","','","'")}.")
   }
 
