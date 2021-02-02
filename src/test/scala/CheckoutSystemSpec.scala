3856dfa798d2b22a5b23e03f52d589601771394b
philipjss <philip.schwarz@sefaira.com>
2021-02-02 19:37:34 +0000

    add test for one apple

--- src/test/scala/CheckoutSystemSpec.scala
+++ src/test/scala/CheckoutSystemSpec.scala
@@ -7,10 +7,13 @@ class CheckoutSystemSpec extends Specification with Matchers {
   "a CheckoutSystem" should {
 
     "compute the cost of zero items" in zero_items_test
-
+    "compute the cost of one apple" in one_apple_test
   }
 
   def zero_items_test: Result =
     CheckoutSystem.checkout(Nil) must beEqualTo(0)
+
+  def one_apple_test: Result =
+    CheckoutSystem.checkout(List("apple")) must beEqualTo(60)
 
 }
