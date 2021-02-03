661789bd8111b5667bed81667948880ea09f9ac1
philipjss <philip.schwarz@sefaira.com>
2021-02-03 11:03:59 +0000

    make the two new tests pass

--- src/test/scala/CheckoutSystem.scala
+++ src/test/scala/CheckoutSystem.scala
@@ -11,8 +11,8 @@ object CheckoutSystem {
     val unrecognisedItems = itemsByType.getOrElse(unrecognisedItem, Nil)
     if (unrecognisedItems.isEmpty) {
       Right(itemsByType.map {
-        case ("apple", apples) => 60 * apples.size
-        case ("orange", oranges) => 25 * oranges.size
+        case ("apple", apples) => 60 * (apples.size / 2 + apples.size % 2)
+        case ("orange", oranges) => 25 * (oranges.size / 3 * 2 + oranges.size % 3)
       }.foldLeft(0)(_ + _))
     } else {
       Left(s"ERROR - the following items were not recognised: ${unrecognisedItems.mkString("'", "','", "'")}.")
