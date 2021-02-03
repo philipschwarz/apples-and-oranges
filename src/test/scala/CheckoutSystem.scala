a90f6b87187ebe314f9bf0c3c4108848f42d3555
philipjss <philip.schwarz@sefaira.com>
2021-02-03 09:20:44 +0000

    make the two new tests pass

--- src/test/scala/CheckoutSystem.scala
+++ src/test/scala/CheckoutSystem.scala
@@ -1,10 +1,22 @@
 object CheckoutSystem {
 
-  def checkout(items: List[String]): Either[String, Int] = items match {
-    case Nil => Right(0)
-    case List("apple") => Right(60)
-    case List("orange") => Right(25)
-    case _ => Left(s"ERROR - the following items were not recognised: ${items.mkString("'","','","'")}.")
+  val unrecognisedItem = "unrecognised-item"
+
+  def checkout(items: List[String]): Either[String, Int] = {
+    val itemsByType: Map[String, List[String]] = items.groupBy {
+      case "apple" => "apple"
+      case "orange" => "orange"
+      case _ => unrecognisedItem
+    }
+    val unrecognisedItems = itemsByType.getOrElse(unrecognisedItem, Nil)
+    if (unrecognisedItems.isEmpty) {
+      Right(itemsByType.map {
+        case ("apple", apples) => 60 * apples.size
+        case ("orange", oranges) => 25 * oranges.size
+      }.foldLeft(0)(_ + _))
+    } else {
+      Left(s"ERROR - the following items were not recognised: ${unrecognisedItems.mkString("'", "','", "'")}.")
+    }
   }
 
 }
