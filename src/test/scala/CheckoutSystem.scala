8b56e1cb8753cad13b0756a314e260b8c9577f64
philipjss <philip.schwarz@sefaira.com>
2021-02-03 13:11:24 +0000

    improve understandability by extracting intention-revealing method names

--- src/test/scala/CheckoutSystem.scala
+++ src/test/scala/CheckoutSystem.scala
@@ -4,24 +4,37 @@ import ItemType._
 object CheckoutSystem {
 
   def checkout(items: List[String]): Either[String, Int] = {
-    val itemsByType: Map[ItemType, List[String]] = items.groupBy {
+    val itemsByType: Map[ItemType, List[String]] = groupItemsByType(items)
+    val unrecognisedItems: List[String] = getUnrecognisedItems(itemsByType)
+    if (unrecognisedItems.isEmpty) {
+      Right(computePrice(itemsByType))
+    } else {
+      Left(s"ERROR - the following items were not recognised: ${unrecognisedItems.mkString("'", "','", "'")}.")
+    }
+  }
+
+  private def groupItemsByType(items: List[String]): Map[ItemType, List[String]] = {
+    items.groupBy {
       case "apple" => Apple
       case "orange" => Orange
       case _ => Unrecognised
     }
-    val unrecognisedItems: List[String] = itemsByType.getOrElse(Unrecognised, Nil)
-    if (unrecognisedItems.nonEmpty) {
-      Left(s"ERROR - the following items were not recognised: ${unrecognisedItems.mkString("'", "','", "'")}.")
-    } else {
-      val priceForEachItemType = itemsByType.map { case (itemType, items) =>
-        val (quantity, price, maybeOffer) = (items.size, itemType.price, itemType.offer)
-          maybeOffer.map{ offer =>
-            price * (quantity / offer.quantity * (offer.quantity - offer.freeQuantity) + quantity % offer.quantity)
-          }.getOrElse(price * quantity)
-      }
-      val priceForAllItemTypes = priceForEachItemType.foldLeft(0)(_ + _)
-      Right(priceForAllItemTypes)
-    }
   }
 
+  private def getUnrecognisedItems(itemsByType: Map[ItemType, List[String]]): List[String] = {
+    itemsByType.getOrElse(Unrecognised, Nil)
+  }
+
+  private def computePrice(itemsByType: Map[ItemType, List[String]]) = {
+    computePriceForEachItemType(itemsByType).foldLeft(0)(_ + _)
+  }
+
+  private def computePriceForEachItemType(itemsByType: Map[ItemType, List[String]]): List[Int] = {
+    itemsByType.map { case (itemType, items) =>
+      val (quantity, price, maybeOffer) = (items.size, itemType.price, itemType.offer)
+      maybeOffer.map { offer =>
+        price * (quantity / offer.quantity * (offer.quantity - offer.freeQuantity) + quantity % offer.quantity)
+      }.getOrElse(price * quantity)
+    }.toList
+  }
 }
